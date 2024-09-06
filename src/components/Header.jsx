30f59d6e1c7fa6612e87d39cdad0dbce1feff661
oreva <orevaawusi@gmail.com>
2024-09-06 04:55:30 +0100

    header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './header.module.css'
 
+
 const Header = () => {
   return (
     <div className={styles.header} >
-      header
+      <h1 className={styles.h1}>Todo App</h1>
+      <h2 className={styles.h2}>Create | Delete | Update Task </h2>
     </div>
   )
 }
